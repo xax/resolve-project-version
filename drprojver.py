153d9ffe3859e303d464b6c9fe02286bb79fdc07
xax <xa@universe>
2026-01-11 20:12:59 +0100

    Allow to patch xml comment app and project versions as well.

--- drprojver.py
+++ drprojver.py
@@ -6,12 +6,14 @@ import re
 from zipfile import ZipFile
 
 __COPYRIGHT__ = "Copyright © by XA, I 2026. All rights reserved."
-__VERSION__ = "1.0.0"
+__VERSION__ = "1.1.0"
 
 
 class patch_xml_projectversion:
     """
     Patch ‹version› into ‹set_ver› in `<ProjectVersion>‹version›</ProjectVersion>` sequence of data_bytes (UTF-8).
+
+    This should not technically be necessary, as the relevant project version is stored in an XML tag.
     """
 
     re_projectversion = re.compile(r"<ProjectVersion>\s*(\d+)\s*</ProjectVersion>")
@@ -36,13 +38,45 @@ class patch_xml_projectversion:
         return data_bytes
 
 
-def process_dr_project(inputfile: Path | str, outputfile: Path | str, *, set_ver: str, assert_ver: str = "") -> None:
+class patch_comment_version_info:
+    """
+    Patch ‹version› tags of DR and project version info in XML comment in any xml file of the project.
+    """
+
+    re = re.compile(r"""<!--\s*DbAppVer="(?P<appver>[\d.]+)"\s+DbPrjVer="(?P<projver>[\d.]+)"\s*-->""")
+
+    def __new__(cls, data_bytes: bytes, *, set_ver: str, assert_ver: str = "", set_appver: str = "") -> bytes:
+        """
+        Construction with `__new__` makes class directly callable (w/o instantiation) and the re compiled only once.
+
+        :param data_bytes: input data bytes
+        :param set_ver: version string to set
+        :param assert_ver: if not empty, assert that existing version matches this string
+        :return: modified data bytes
+        """
+        data = data_bytes.decode()
+        m = cls.re.search(data)
+        if m is not None:
+            if assert_ver != "" and m.group("projver") is not None and m.group("projver") != str(assert_ver):
+                raise AssertionError({"ver": str(m.group("projver"))})
+            else:
+                data = data[: m.start("projver")] + str(set_ver) + data[m.end("projver") :]
+                if set_appver != "":
+                    data = data[: m.start("appver")] + str(set_appver) + data[m.end("appver") :]
+                data_bytes = data.encode()
+        return data_bytes
+
+
+def process_dr_project(
+    inputfile: Path | str, outputfile: Path | str, *, set_ver: str, assert_ver: str = "", set_appver: str = ""
+) -> None:
     """
     Process Davinci Resolve® project file (.drp) to set project version.
 
     :param inputfile: input project file path
     :param outputfile: output project file path
     :param set_ver: version string to set
+    :param set_appver: application version string to set in comment info
     :param assert_ver: if not empty, assert that existing version matches this string
     """
 
@@ -64,9 +98,17 @@ def process_dr_project(inputfile: Path | str, outputfile: Path | str, *, set_ver
                                 f"Project version {e.args[0]["ver"]} in file is not expected version {assert_ver}. Skipping modification."
                             )
                             pass
-                    elif False:
-                        # patch whatever
-                        pass
+                    if set_appver != "" and in_item_name.filename.endswith(".xml"):
+                        # patch comment version info
+                        try:
+                            data_bytes = patch_comment_version_info(
+                                data_bytes, set_ver=set_ver, assert_ver=assert_ver, set_appver=set_appver
+                            )
+                        except AssertionError as e:
+                            print(
+                                f"Project version {e.args[0]['ver']} in file is not expected version {assert_ver}. Skipping modification."
+                            )
+                            pass
 
                     # copy item over
                     out_project.writestr(in_item_name, data_bytes)
@@ -81,9 +123,14 @@ def parse_arguments() -> argparse.Namespace:
         suggest_on_error=True,
     )
 
-    parser.add_argument("-s", "--setver", type=str, required=True, help="version string to write into project file")
+    parser.add_argument(
+        "-s", "--setver", type=str, required=True, help="project version string to write into project file"
+    )
     parser.add_argument(
         "-a", "--assertver", type=str, default="", help="assert that existing project file bears given version"
+    )
+    parser.add_argument(
+        "-A", "--appver", type=str, default="", help="DR application version string to write into project file comments"
     )
     parser.add_argument("input", type=Path, help="input project file (.drp)")
     parser.add_argument("output", type=Path, help="modified output project file (.drp)")
@@ -95,7 +142,7 @@ def main():
     """Main function."""
     args: argparse.Namespace = parse_arguments()
 
-    process_dr_project(args.input, args.output, set_ver=args.setver, assert_ver=args.assertver)
+    process_dr_project(args.input, args.output, set_ver=args.setver, assert_ver=args.assertver, set_appver=args.appver)
 
 
 if __name__ == "__main__":
